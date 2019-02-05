2681fdc39984b8ccc6c42a15906053ee8b299f10
Alep <ralem31@gmail.com>
2019-02-05 16:51:24 -0600

    Feat(*): Add info store

---
+++ src/app/actions/actions_info.js
@@ -0,0 +1,3 @@
+export const INFO_CURRENT_SET = "INFO_CURRENT_SET";
+
+export const infoCurrentSet = payload => ({ type: INFO_CURRENT_SET, payload });
--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import app from "./reducer_app";
 import captured from "./reducer_captured";
+import info from "./reducer_info";
 
 const rootReducer = combineReducers({
     app,
-    captured
+    captured,
+    info
 });
 
 export default rootReducer;
